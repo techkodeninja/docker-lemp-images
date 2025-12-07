68f68222b895c0c64fddfb215d3963cd91a82796
techkodeninja <164923145+techkodeninja@users.noreply.github.com>
2025-12-06 22:43:18 -0800

    update

--- 8.2/scripts/php.sh
+++ 8.2/scripts/php.sh
@@ -1,30 +1,30 @@
 #!/bin/sh -eux
 
-# Here, we are going to install PHP 8.1
+# Here, we are going to install PHP 8.2
 apt-get install --no-install-recommends \
-    php8.1-bcmath \
-    php8.1-cli \
-    php8.1-common \
-    php8.1-curl \
-    php8.1-dev \
-    php8.1-fpm \
-    php8.1-gd \
-    php8.1-imap \
-    php8.1-imagick \
-    php8.1-imap \
-    php8.1-intl \
-    php8.1-mbstring \
-    php8.1-mysql \
-    php8.1-pcov \
-    php8.1-ssh2 \
-    php8.1-soap \
-    php8.1-xml \
-    php8.1-yaml \
-    php8.1-zip \
+    php8.2-bcmath \
+    php8.2-cli \
+    php8.2-common \
+    php8.2-curl \
+    php8.2-dev \
+    php8.2-fpm \
+    php8.2-gd \
+    php8.2-imap \
+    php8.2-imagick \
+    php8.2-imap \
+    php8.2-intl \
+    php8.2-mbstring \
+    php8.2-mysql \
+    php8.2-pcov \
+    php8.2-ssh2 \
+    php8.2-soap \
+    php8.2-xml \
+    php8.2-yaml \
+    php8.2-zip \
     -y
 
 # Here, we are going to create default directory for php
 mkdir -p /run/php
 
-# Here we are going to make sure that PHP 8.1 is default
-sudo update-alternatives --set php /usr/bin/php8.1
+# Here we are going to make sure that PHP 8.2 is default
+sudo update-alternatives --set php /usr/bin/php8.2
